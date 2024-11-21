ca711d0367fed20a5d2c9e56ec031121223494ec
Piotr K. Wyrwas <pw.piotr.wyrwas@gmail.com>
2024-11-21 14:08:46 +0100

    Invoke openapi generator

--- src/app/openapi/model/topicCreationRequest.ts
+++ src/app/openapi/model/topicCreationRequest.ts
@@ -13,5 +13,6 @@
 export interface TopicCreationRequest { 
     name: string;
     description: string;
+    color: string;
 }
 
--- src/app/openapi/model/updateTopicRequest.ts
+++ src/app/openapi/model/updateTopicRequest.ts
@@ -14,5 +14,6 @@ export interface UpdateTopicRequest {
     id: string;
     name?: string;
     description?: string;
+    color?: string;
 }
 
